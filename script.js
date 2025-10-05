118bcce37049c44c9a4e929c47871dd269896110
Hussain007H <143806978+Hussain007H@users.noreply.github.com>
2025-10-05 14:01:06 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const startBtn = document.getElementById("start");
+const pauseBtn = document.getElementById("pause");
+const resumeBtn = document.getElementById("resume");
+const bgInput = document.getElementById("backgroundInput");
